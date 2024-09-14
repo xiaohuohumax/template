58af2d34f0dcce139b09a60592e623bc3052309d
xiaohuohumax <1915970986@qq.com>
2024-09-14 15:20:38 +0800

    feat: :sparkles: 添加环境变量本地运行

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 # node-github-action-template
 
+## 1.1.0
+
+### Minor Changes
+
+- 添加环境变量本地运行
+
 ## 1.0.0
 
 ### Major Changes
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,21 @@
 import * as core from '@actions/core';
+import * as github from '@actions/github';
 
-/**
- * Main function
- */
 async function main(): Promise<void> {
+  // 普通输入
   const message = core.getInput('message');
-  console.log(`Get input message: ${message}`);
+  core.info(`message: ${message}`);
+
+  // 仓库信息
+  const { owner, repo } = github.context.repo;
+  core.info(`owner: ${owner}, repo: ${repo}`);
+
+  // 访问 GitHub API
+  // const token = core.getInput('token');
+  // const octokit = github.getOctokit(token);
+  // core.info(`token: ${token}, octokit: ${octokit}`);
+
+  // 输出
   core.setOutput('result', new Date().toISOString());
 }
 
